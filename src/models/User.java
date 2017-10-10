71ab78387a09109a17c088d289a1f0e0204823e7
20075884 <20075884@IT101PC15.wit.ie>
2017-10-10 14:58:19 +0100

    Finished Junit test included

--- src/models/User.java
+++ src/models/User.java
@@ -1,7 +1,12 @@
 package models;
+import com.google.common.base.Objects;
 import static com.google.common.base.MoreObjects.toStringHelper;
 public class User
 {	
+	static Long   counter = 0l;
+
+	  public Long   id;
+
 	public String firstName;
 	  public String lastName;
 	  public String email;
@@ -12,20 +17,31 @@ public class User
 	  }
 	  public User(String firstName, String lastName, String email, String password)
 	  {
+		   this.id        = counter++;
+
 	    this.firstName = firstName;
 	    this.lastName = lastName;
 	    this.email = email;
 	    this.password = password;
 	  }
 	  
+	  
+	  @Override  
+	  public int hashCode()  
+	  {  
+	     return Objects.hashCode(this.lastName, this.firstName, 
+	    		 this.email, this.password);  
+	  }
+	  
+	  @Override
 	  public String toString()
 	  {
-	    return toStringHelper(this).addValue(firstName)
+	    return toStringHelper(this) .addValue(id)
+	    		                    .addValue(firstName)
 	                               .addValue(lastName)
 	                               .addValue(password)
 	                               .addValue(email)                               
 	                               .toString();
 	  }
-	  
 	  
 	}
